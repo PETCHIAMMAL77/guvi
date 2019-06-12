2a36e3ab21d0fdb7511c6f679f858060207bb7e8
PETCHIAMMAL77 <51688839+PETCHIAMMAL77@users.noreply.github.com>
2019-06-12 11:55:07 +0530

    Create vowel.py

---
+++ codekata/vowel.py
@@ -0,0 +1,7 @@
+vl=input()
+if(vl=='A' or vl=='E' or vl=='I' or vl=='O' or vl=='U' or vl=='a' or vl=='e' or vl=='i' or vl=='o' or vl=='u'):
+  print("Vowel")
+elif(vl=='~' or vl=='!' or vl=='@' or vl=='#' or vl=='$' or vl=='%' or vl=='^' or vl=='&' or vl=='*'):
+  print("invalid")
+else:
+  print("Consonant")
